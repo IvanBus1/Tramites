7f420374e0d8efb8c5ffe616f893c9609dfd4f45
Ivan Bustillos <IvanBus233188>
2023-04-08 11:49:33 -0600

    a

--- target/generated-sources/annotations/Entidades/Licencia_.java
+++ target/generated-sources/annotations/Entidades/Licencia_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-04-06T13:20:46", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-04-07T22:12:22", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Licencia.class)
 public class Licencia_ extends Tramite_ {
 
--- target/generated-sources/annotations/Entidades/Persona_.java
+++ target/generated-sources/annotations/Entidades/Persona_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-04-06T13:20:46", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-04-07T22:12:22", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
